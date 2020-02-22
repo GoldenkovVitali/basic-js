3643a40e06b420064381319187eeae4935eeec4b
GoldenkovVitali <vitalik.resscuer@mail.ru>
2020-02-22 17:56:36 +0300

    modified:   src/carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,14 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
-
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+const k= 0.693/HALF_LIFE_PERIOD;
+module.exports = function dateSample(sampleActivity) {
+console.log (sampleActivity);
+sampleActivity = typeof sampleActivity === "string" && parseFloat(sampleActivity);
+console.log (sampleActivity);
+console.log(Math.ceil(Math.log(MODERN_ACTIVITY/Number(sampleActivity))/k));
+if (!isNaN(sampleActivity) && sampleActivity>0 && sampleActivity<15) {
+console.log (Math.log(MODERN_ACTIVITY/Number(sampleActivity))/k)
+return Math.ceil(Math.log(MODERN_ACTIVITY/Number(sampleActivity))/k);
+}
+return false;
+};  
