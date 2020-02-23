3df9abf1249c1c2fac829920d451a990559ac007
GoldenkovVitali <vitalik.resscuer@mail.ru>
2020-02-23 17:50:41 +0300

    modified:   src/count-cats.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,13 +2,10 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 const k= 0.693/HALF_LIFE_PERIOD;
 module.exports = function dateSample(sampleActivity) {
-console.log (sampleActivity);
 sampleActivity = typeof sampleActivity === "string" && parseFloat(sampleActivity);
-console.log (sampleActivity);
-console.log(Math.ceil(Math.log(MODERN_ACTIVITY/Number(sampleActivity))/k));
 if (!isNaN(sampleActivity) && sampleActivity>0 && sampleActivity<15) {
-console.log (Math.log(MODERN_ACTIVITY/Number(sampleActivity))/k)
-return Math.ceil(Math.log(MODERN_ACTIVITY/Number(sampleActivity))/k);
+let sample = Math.ceil(Math.log(MODERN_ACTIVITY/Number(sampleActivity))/k)
+return sample;
 }
 return false;
 };  
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function countCats(matrix) {
+  var matrix = matrix.flat();
+  let cat= '^^';
+  let catNum=0;
+  for (let i=0; i<matrix.length; i++){
+  if (matrix [i]==cat) {
+  catNum++;
+  }
+  }
+  return catNum;
+ };
