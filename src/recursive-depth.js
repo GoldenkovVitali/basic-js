bede9f9a1d9e3ac14749245666a61a4536d3cb21
GoldenkovVitali <vitalik.resscuer@mail.ru>
2020-02-23 21:46:06 +0300

    modified:   src/recursive-depth.js

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,11 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+calculateDepth(arr, level) {
+var level=1
+if(arr.some(arrel=>Array.isArray(arrel))) //проверяет, удовлетворяет ли какой-либо элемент массива условию, заданному в передаваемой функции
+{
+let arr1 = arr.flat(); //возвращает новый массив, в котором все элементы вложенных подмассивов были рекурсивно "подняты" на указанный уровень level
+return  this.calculateDepth(arr1) + level; // выводит значение функции (this) + 1
+}
+return level;
+}
 };
