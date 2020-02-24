b62b13408c3acaf83fa33ef1049e5d4a81f1e57c
GoldenkovVitali <vitalik.resscuer@mail.ru>
2020-02-24 22:34:30 +0300

    modified:   src/vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,45 +1,45 @@
 class VigenereCipheringMachine {
-constructor(type = true) {
-if (this.type = type){
-this.type = 'direct' 
-}
-else {
-this.type = 'reverse'
-}
-}
+    constructor(type = true) {
+    if (this.type = type){
+    this.type = 'direct' 
+    }
+    else {
+    this.type = 'reverse'
+    }
+    }
 
 encrypt(message, key) {
     if (!key || !message) {
-    throw Error()
+    throw Error();
     }
     key = key.toUpperCase();
     message = message.toUpperCase();
     let q=0;
-    let newmessage = ''
+    let newmessage = '';
     for (let i = 0; i < message.length; i++) {
     if (message[i].match(/^[A-Z]$/)) {
     newmessage += String.fromCharCode(((message[i].charCodeAt() + key[q % key.length].charCodeAt()) % 26) + 97)
     q=q+1;
     }
     else {
-    newmessage = newmessage+message[i]
+    newmessage = newmessage+message[i];
     }
     }
     if (this.type === 'reverse'){
     return newmessage.split('').reverse().join('').toUpperCase();
     }
     if (this.type === 'direct'){
-    return  newmessage.toUpperCase()
+    return  newmessage.toUpperCase();
     }
     }
 
     decrypt(newmessage, key) {
     if (!key || !newmessage) {
-    throw Error()
+    throw Error();
     }
-    newmessage = newmessage.toUpperCase()
-    key = key.toUpperCase()
-    let result = ''
+    newmessage = newmessage.toUpperCase();
+    key = key.toUpperCase();
+    let result = '';
     let i=0;
     let q=0;
     for (i; i < newmessage.length; i++) {
@@ -50,19 +50,19 @@ encrypt(message, key) {
     q=q+1;
     } 
     else {
-    result = result+String.fromCharCode((26 + inconsistencies % 26) + 97)
+    result = result+String.fromCharCode((26 + inconsistencies % 26) + 97);
     q=q+1;
     }
     } 
     else {
-    result = result+newmessage[i]
+    result = result+newmessage[i];
     }
     }
     if (this.type === 'reverse'){
     return result.split('').reverse().join('').toUpperCase();
     }
     if (this.type === 'direct'){
-    return  result.toUpperCase()
+    return  result.toUpperCase();
     }
     }
 }
