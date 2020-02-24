3181f3569278101ede4bf0984fac3091fee39919
GoldenkovVitali <vitalik.resscuer@mail.ru>
2020-02-24 20:15:58 +0300

    modified:   src/hanoi-tower.js

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,5 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  turns= Math.pow(2, disksNumber) - 1; // 2^N - 1
+  seconds = turns / (turnsSpeed / 3600); //speed
+  return {turns, seconds}
 }
